1c717ba9b9a3b4bd534bbafe7174fd293bf1f609
Boonyasuk Dheeravongkit <boonyasuk.d@gmail.com>
2020-04-22 00:34:20 +0700

    typescript prop typing isn't available yet; fall back for now

--- src/components/FormActions.vue
+++ src/components/FormActions.vue
@@ -7,13 +7,11 @@
 </template>
 
 <script lang="ts">
-import { createComponent, Ref } from '@vue/composition-api';
+import { defineComponent } from 'vue';
 import { useI18n } from '../composites/base/i18n';
 
-interface Props { valid: Ref<boolean>, save: () => void, reset: () => void }
-
-export default createComponent<Props>({
-  props: { valid: {}, save: {}, reset: {} }, // needed for vue 2.x
+export default defineComponent({
+  props: ['valid', 'save', 'reset'],
   setup() {
     const { msg } = useI18n();
     return { msg };
--- src/components/FormItem.vue
+++ src/components/FormItem.vue
@@ -13,14 +13,12 @@
 </template>
 
 <script lang="ts">
-import { createComponent } from '@vue/composition-api';
+import { defineComponent } from 'vue';
 import { useFormFieldManager } from '../composites/form_management';
 
-interface Props { fieldName: string }
-
-export default createComponent<Props>({
-  props: { fieldName: {} }, // needed for vue 2.x
-  setup(props: Props) {
+export default defineComponent({
+  props: ['fieldName'],
+  setup(props) {
     const { value, label, error } = useFormFieldManager(props.fieldName);
     return { value, label, error };
   },
